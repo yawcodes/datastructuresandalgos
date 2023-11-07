285ed58a4aa3460dc2d2ef8450dc5ece4438b3cb
yawcodes <yawoseiagyemang8@gmail.com>
2023-11-06 20:27:43 -0500

    Adding Node implementation

---
+++ src/src/lineardatastructures/Node.java
@@ -0,0 +1,21 @@
+package lineardatastructures;
+
+public class Node {
+    public String data;
+    private Node next;
+
+    public Node(String data) {
+        this.data = data;
+        this.next = null;
+    }
+
+
+    public Node getNextNode() {
+        return this.next;
+    }
+
+    public void setNextNode(Node node) {
+        this.next = node;
+    }
+
+}
