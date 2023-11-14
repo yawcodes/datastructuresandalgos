ef9ac0ccc4765afa9897073b168c51778e2bc82b
yawcodes <yawoseiagyemang8@gmail.com>
2023-11-13 21:57:22 -0500

    feature/Tree-and-TreeNode-implementation - Adding Tree and TreeNode implementations.

---
+++ src/src/nonlineardatastructures/Tree.java
@@ -0,0 +1,24 @@
+package nonlineardatastructures;
+
+public class Tree {
+    public TreeNode root;
+
+    public Tree(TreeNode root) {
+        this.root = root;
+    }
+
+    public void print() {
+        print(this.root, 0);
+    }
+
+    public void print(TreeNode current, int level) {
+        String levelMark = "";
+        for (int i = 0; i < level; i ++) {
+            levelMark += "-- ";
+        }
+        System.out.println(levelMark + " " + current.data);
+        for (TreeNode child : current.children) {
+            print(child, level + 1);
+        }
+    }
+}
